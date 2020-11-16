b87fec161055ff49b1a179f858d17dc32d85fcbb
suica26 <m011931038@edu.teu.ac.jp>
2020-11-16 18:05:44 +0900

    Script移行の作業分です。 以前のScriptは以下のように紐づいてます。
    BuildingBreak_Y //一度スコア関連の処理を外してます
    -BillBreak
    -HouseBreak
    -JihankiBreak
    -SoukoBreak
    -TankeBreak
    -TreeBreak
    
    EnemyNav_Y
    -Enemy
    
    TankBullet_Y
    -launchport
    -Bullet
    
    MorningBlast_Y
    -MoveSphere
    -sphere

---
+++ Assets/Yamamoto/Scripts/EnemyNav_Y.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNav_Y : MonoBehaviour
+{
+    //これは敵に付けてください
+    public GameObject player;
+    private NavMeshAgent nav;
+    private Vector3 targetPos;
+    public float eneDis = 20.0f;//追加
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = this.gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPos = player.transform.position;
+        //この下変更＆追加
+        if (Vector3.Distance(targetPos, this.transform.position) <= eneDis)
+        {
+            nav.enabled = true;
+            nav.destination = targetPos;
+        }
+        else
+        {
+            nav.enabled = false;
+
+        }
+    }
+}
