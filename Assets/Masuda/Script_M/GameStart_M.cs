6f299f8a5482076d5722c3e5b00c76421f1a1e10
ryuga-masuda <m0119260b1@edu.teu.ac.jp>
2020-12-05 14:36:27 +0900

    スタート画面の設定とリザルト画面の作成

---
+++ Assets/Masuda/Script_M/GameStart_M.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStart_M : MonoBehaviour
+{
+    //タイトル画面でボタンをクリックするとゲーム画面を読み込みます
+    //シーンGameStartのButtonに張っ付いてます
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnStart()
+    {
+        SceneManager.LoadScene("GameScene_M");
+    }
+}
---
+++ Assets/Masuda/Script_M/Gauge_M.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gauge_M : MonoBehaviour
+{
+    //仮で作ったゲージ
+    RectTransform rt;
+
+    void Start()
+    {
+        rt = GetComponent<RectTransform>();
+    }
+
+    
+    void Update()
+    {
+        //Spaceを押すとゲージがたまっていく
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rt.sizeDelta = new Vector2(rt.sizeDelta.x + 2.5f, 25.0f);
+            if (rt.sizeDelta.x >= 150)
+            {
+                //たぶん1秒でマックスまで行く
+                rt.sizeDelta = new Vector2(150.0f, 25.0f);
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            //Spaceを離すとゲージが空になる
+            rt.sizeDelta = new Vector2(0.0f, 50.0f);
+        }
+    }
+}
