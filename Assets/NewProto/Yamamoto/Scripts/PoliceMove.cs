032a5cad30f632bf995f9e8371bbc362a763cafb
suica26 <m011931038@edu.teu.ac.jp>
2021-02-14 00:48:36 +0900

    とりあえずScript保存 作ったモデルはいらない子ですね（）

--- Assets/NewProto/Yamamoto/Scripts/PoliceMove.cs
+++ Assets/NewProto/Yamamoto/Scripts/PoliceMove.cs
@@ -7,8 +7,8 @@ public class PoliceMove : MonoBehaviour
     private EnemyNav_Y navScript;
     private float routineTimer = 0f;
     //攻撃をした際の次の行動までの時間
-    public float fireFreeze = 5f;    //発砲時
-    public float hitFleeze = 2f;    //警棒で殴った時
+    public float fireFreeze = 3f;    //発砲時
+    public float hitFleeze = 1f;    //警棒で殴った時
     public GameObject bulletPrefab = null;
     // Start is called before the first frame update
     void Start()
