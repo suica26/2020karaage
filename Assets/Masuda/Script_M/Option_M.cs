0db7e7d2c7721e7cbe9ac166af1aa6c714e7167e
Yuetatsu <m0119140ba@edu.teu.ac.jp>
2021-02-20 05:00:21 +0900

    増田氏のスクリプトを再現。アセットは後でパッケージ化するなりして追加してくれ～

---
+++ Assets/Masuda/Script_M/Option_M.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Option_M : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnClick()
+    {
+        pausePanel.SetActive(true);
+    }
+}
--- Assets/NewProto/SASAKI/Scripts/Parameters_R.cs
+++ Assets/NewProto/SASAKI/Scripts/Parameters_R.cs
@@ -28,19 +28,24 @@ public class Parameters_R : MonoBehaviour
     [SerializeField] private Text scoreText, timeText, epText, hpText;
     [SerializeField] private GameObject resultPanel = null;
 
-    [SerializeField] public int score, time, ep, hp;
-
+    [SerializeField] public int score, time, ep, hp, maxEP1, maxEP2, maxEP3, maxHP1, maxHP2, maxHP3, plusHP1, plusHP2, plusHP3, sp;
+    [SerializeField] public Slider hpSlider, epSlider, kickSlider;
 
     private bool freeze = false;
     private float count;
 
     void Start()
     {
-        scoreText.text = "Score: " + score;
+        scoreText.text = "Price: $" + score;
         timeText.text = "Time: " + time;
         epText.text = "EP: " + ep;
         hpText.text = "HP: " + hp;
-
+        hpSlider.value = 50;
+        hpSlider.maxValue = 50;
+        epSlider.value = 0;
+        epSlider.maxValue = 30;
+        kickSlider.value = 0;
+        kickSlider.maxValue = 10;
         count = time;
     }
 
@@ -49,7 +54,15 @@ public class Parameters_R : MonoBehaviour
         if (!freeze)
         {
             score += addScore;
-            scoreText.text = "Score: " + score;
+            scoreText.text = "Price: $" + score;
+            kickSlider.value += 1;
+            if (Input.GetMouseButton(0))
+            {
+                if(kickSlider.value == sp)
+                {
+                    kickSlider.value = 0;
+                }
+            }
         }
     }
     //引数で指定した分だけスコアを加算します。
@@ -76,6 +89,18 @@ public class Parameters_R : MonoBehaviour
         {
             ep += addEP;
             epText.text = "EP: " + ep;
+            if(ep == 30)
+            {
+                TimeManager(10);
+            }else if(ep == 100)
+            {
+                TimeManager(10);
+            }else if(ep == 300)
+            {
+                TimeManager(10);
+            }
+            TimeManager(1);
+            epSlider.value += addEP;
         }
     }
     //引数で指定した分だけEPを加算します。
@@ -85,6 +110,7 @@ public class Parameters_R : MonoBehaviour
         if (!freeze)
         {
             hp -= addHP;
+            hpSlider.value -= addHP;
             if (hp <= 0)
             {
                 freeze = true;
@@ -102,6 +128,28 @@ public class Parameters_R : MonoBehaviour
         if (time - count > 1)
         {
             TimeManager(-1);
+        }
+
+        if (epSlider.value == maxEP1)
+        {
+            epSlider.value = 0;
+            epSlider.maxValue = maxEP2;
+            maxEP1 = 10000;
+            hpSlider.maxValue = maxHP1;
+            hpSlider.value += plusHP1;
+        }
+        else if (epSlider.value == maxEP2)
+        {
+            epSlider.value = 0;
+            epSlider.maxValue = maxEP3;
+            maxEP2 = 10000;
+            hpSlider.maxValue = maxHP2;
+            hpSlider.value += plusHP2;
+        }
+        else if (epSlider.value == maxEP3)
+        {
+            hpSlider.maxValue = maxHP3;
+            hpSlider.value += plusHP3;
         }
     }
     //タイマーです。一秒ごとにTimeManager()で一秒減らしてます。
