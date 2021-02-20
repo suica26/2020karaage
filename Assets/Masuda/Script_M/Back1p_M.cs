542d09ccb2dc654d8d32f2fe6452111d1861c0cd
ryuga-masuda <m0119260b1@edu.teu.ac.jp>
2021-02-20 22:27:11 +0900

    今度こそいけるはず

---
+++ Assets/Masuda/Script_M/Back1p_M.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Back1p_M : MonoBehaviour
+{
+    [SerializeField] private GameObject optionPanel1, optionPanel2;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        optionPanel1.SetActive(true);
+        optionPanel2.SetActive(false);
+    }
+}
---
+++ Assets/Masuda/Script_M/NextOptio_M.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextOptio_M : MonoBehaviour
+{
+    [SerializeField] private GameObject optionPanel1, optionPanel2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        optionPanel1.SetActive(false);
+        optionPanel2.SetActive(true);
+    }
+}
