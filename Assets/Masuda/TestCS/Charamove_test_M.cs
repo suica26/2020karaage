cf072231b8862228d791fbc59f27b22259b0d60e
ryuga-masuda <m0119260b1@edu.teu.ac.jp>
2021-03-23 23:42:42 +0900

    個別方式でスコアを出せるように一応なった

---
+++ Assets/Masuda/TestCS/Charamove_test_M.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charamove_test_M : MonoBehaviour
+{
+    private Vector3 latestPos;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 diff = transform.position - latestPos;
+        latestPos = transform.position;
+
+        if (diff.magnitude > 0.01f)
+        {
+            transform.rotation = Quaternion.LookRotation(diff);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, 3 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -3 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(3 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-3 * Time.deltaTime, 0, 0);
+        }
+    }
+}
