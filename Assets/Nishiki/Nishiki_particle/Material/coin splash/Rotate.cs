ff5412002cff1f6426d717c3123f72847b6921fc
nishiki3160 <65889937+nishiki3160@users.noreply.github.com>
2021-03-28 12:38:14 +0900

    エフェクト９種類追加

---
+++ Assets/Nishiki/Nishiki_particle/Material/coin splash/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+	float angle;
+	public float speed;
+
+	void Update()
+	{
+
+		angle = angle + speed;
+
+		transform.eulerAngles = new Vector3(0, angle, 0);
+	}
+}
---
+++ Assets/Nishiki/Nishiki_particle/Material/coin splash/itemer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemer : MonoBehaviour
+{
+    public GameObject coin;
+    public GameObject smoke;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(coin, transform.position, coin.transform.rotation);
+        Instantiate(smoke, transform.position, smoke.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
