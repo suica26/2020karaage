c48363a8177cca3d8e67109e9a353fb4026d0868
ryuga-masuda <m0119260b1@edu.teu.ac.jp>
2021-05-13 22:17:23 +0900

    さらに変更

--- Assets/Masuda/Script_M/Option/ChangeEnglish.cs
+++ Assets/Masuda/Script_M/Option/ChangeEnglish.cs
@@ -5,15 +5,31 @@ using UnityEngine.UI;
 
 public class ChangeEnglish : MonoBehaviour
 {
-    [SerializeField] public Text start1, start2, howto, option;
+    [SerializeField]
+    public Text text1, text2, text3, text4, text5, text6, text7,
+        text8, text9, text10, text11;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnClick()
+    {
+        text1.text = "Score Attack";
+        text2.text = "Story Mode";
+        text3.text = "How to Play";
+        text4.text = "Option";
+        text5.text = "Option";
+        text6.text = "Quality";
+        text7.text = "Low";
+        text8.text = "High";
+        text9.text = "Language";
+        text10.text = "Japanese";
+        text11.text = "Title";
     }
 }
