5af3be83b04c32536978bb2755927e29b55096b8
Yuetatsu <m0119140ba@edu.teu.ac.jp>
2021-05-16 13:51:22 +0900

    エフェクトを実装

---
+++ Assets/NewProto/SASAKI/Scripts/Gimmick/AirVent_R.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirVent_R : BlowerGimmickBase
+{
+    [SerializeField] private float _minIntervalTime;
+    [SerializeField] private float _maxIntervalTime;
+    private float intervalTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        intervalTime = Random.Range(_minIntervalTime, _maxIntervalTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        intervalTime -= Time.deltaTime;
+        if (intervalTime <= 0f)
+        {
+            intervalTime = Random.Range(_minIntervalTime, _maxIntervalTime);
+            InstanceObject();
+            InstanceEffect();
+        }
+    }
+}
---
+++ Assets/NewProto/SASAKI/Scripts/Gimmick/Manhole_R.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manhole_R : BlowerGimmickBase
+{
+    private GameObject _nullCheckObj;
+    private void OnTriggerEnter(Collider other)
+    {
+        //マンホールのギミック        
+        if (other.gameObject.name == "fallAttackCircle(Clone)"  && _nullCheckObj == null)       
+        {
+            _nullCheckObj = InstanceObject();
+            InstanceEffect();
+        }
+    }
+}
