258cb526f2955d2fc79b5872a85f0b431d167623
ryuga-masuda <m0119260b1@edu.teu.ac.jp>
2021-05-17 00:57:51 +0900

    タイトルをさらに改良しました

---
+++ Assets/Masuda/Script_M/FirstButToSecondBut_M.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstButToSecondBut_M : MonoBehaviour
+{
+    [SerializeField] public GameObject firstButtons, nexts;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        firstButtons.SetActive(false);
+        nexts.SetActive(true);
+    }
+}
