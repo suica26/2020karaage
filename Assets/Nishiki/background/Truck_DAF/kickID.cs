e4211d7900a80fc552302abb7cba572a982433ca
nishiki3160 <65889937+nishiki3160@users.noreply.github.com>
2021-05-16 14:19:34 +0900

    stage0進行中

---
+++ Assets/Nishiki/background/Truck_DAF/kickID.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kickID : MonoBehaviour
+{
+
+    public int id;
+    int time;
+    public float cool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && id == 0)
+        {
+
+            id = 1;
+
+        }
+
+        if (id == 1)
+        {
+
+            time = time + 1;
+
+        }
+
+        if (id == 1 && time >= cool)
+        {
+
+            time = 0;
+            id = 0;
+
+        }
+    }
+}
