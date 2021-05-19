029f668065b70f5c0113da35088503fb640fade8
nishiki3160 <65889937+nishiki3160@users.noreply.github.com>
2021-05-19 21:01:44 +0900

    貰ったfbxトラックを導入＋カッターとブラストOFF（ステージ１）

---
+++ Assets/Nishiki/background/stage0/clearsettings.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clearsettings : MonoBehaviour
+{
+
+    public GameObject whiteness;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+            whiteness.SetActive(true);
+
+        }
+    }
+}
