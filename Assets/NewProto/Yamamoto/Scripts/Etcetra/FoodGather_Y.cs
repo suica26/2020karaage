9e0ea80286136aff3b40216d9cbe12a1011a7e03
suica26 <m011931038@edu.teu.ac.jp>
2021-06-06 04:40:30 +0900

    Scene0からPrologue2への遷移を設定 餌の挙動を改善

--- Assets/NewProto/Yamamoto/Scripts/Etcetra/FoodGather_Y.cs
+++ Assets/NewProto/Yamamoto/Scripts/Etcetra/FoodGather_Y.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FoodGather : MonoBehaviour
+public class FoodGather_Y : MonoBehaviour
 {
     private GameObject player;
     private EvolutionChicken_R scrEvo;
     private Rigidbody rb;
+    public float gatherSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +20,7 @@ public class FoodGather : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(DistanceCul() < 5f)
+        if (DistanceCul() < 20f)
         {
             GoToPlayer();
         }
@@ -27,12 +28,15 @@ public class FoodGather : MonoBehaviour
 
     private float DistanceCul()
     {
-        return(player.transform.position - transform.position).magnitude - 5f * scrEvo.EvolutionNum;
+        return (player.transform.position - transform.position).magnitude - 5f * scrEvo.EvolutionNum;
     }
 
     private void GoToPlayer()
     {
-        Vector3 gatherPower = (player.transform.position - transform.position) * 5f;
-        rb.AddForce(gatherPower,ForceMode.Acceleration);
+        Vector3 toPlayer = (player.transform.position - transform.position).normalized;
+        transform.forward = toPlayer;
+        //速度のY成分を保持(一応)
+        var currentSpeed_Y = new Vector3(0, rb.velocity.y, 0);
+        rb.velocity = toPlayer * gatherSpeed * Time.deltaTime + currentSpeed_Y;
     }
 }
