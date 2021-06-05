0610554a1361a8ffdcf4034ed023c4c30b0ee606
ryuga-masuda <m0119260b1@edu.teu.ac.jp>
2021-06-06 00:14:04 +0900

    スタート画面のボタンの設定とミッションの表示スピードの微調整とparameter_rのちょっと変更

---
+++ Assets/Masuda/Script_M/ButtonRange_M.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace UnityEngine.UI
+{
+    public class ButtonRange_M : Graphic
+    {
+        
+    }
+}
