24902742aae080c4d5ed0dcc7a775f5fa76c6023
ryuga-masuda <m0119260b1@edu.teu.ac.jp>
2021-06-07 19:57:07 +0900

    ボタンの改善

--- Assets/Masuda/StoryCS_M/Stage1_M/Stage1_Mission_M.cs
+++ Assets/Masuda/StoryCS_M/Stage1_M/Stage1_Mission_M.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 
 public class Stage1_Mission_M : MonoBehaviour
 {
-    [SerializeField] public Text mission, submis,count;
+    [SerializeField] public Text mission, submis, exmis, count;
     [SerializeField] public GameObject player, shop, misBox, company;
     [SerializeField] public TextAsset txtFile;
     [SerializeField] public int smallNum, bigNum;
     [SerializeField] public int smallBorder1, smallBorder2, smallBorder3,
-                                bigBorder1, bigBorder2, bigBorder3;
+                                bigBorder1, bigBorder2, bigBorder3,bigBorder4;
     private bool check = false, border = false, first = true, Language;
     private string txtData;
     private string[] splitText;
@@ -30,80 +30,65 @@ public class Stage1_Mission_M : MonoBehaviour
         Vector3 comPos = company.transform.position;
         float dis = Vector3.Distance(playerPos, comPos);
 
-        //本当はshopが消えたら発動する、!shopに直す
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            misBox.SetActive(true);
-            missionSlide.Play();
-            mission.text = splitText[0];
-            submis.text = splitText[1];
-            count.text = "1";
-            border = true;
-        }
-
         if (!shop && first)
         {
             misBox.SetActive(true);
             missionSlide.Play();
             mission.text = splitText[0];
             submis.text = splitText[1];
+            exmis.text = splitText[2];
             count.text = "1";
             first = false;
             border = true;
         }
 
-        if (bigNum >= bigBorder3 && border == true)
+        if (bigNum >= bigBorder4 && border == true)
         {
             missionSlide.Play();
-            mission.text = splitText[2];
-            submis.text = splitText[3];
+            mission.text = splitText[3];
+            submis.text = splitText[4];
+            exmis.text = splitText[5];
             count.text = "2";
             border = false;
             check = true;
         }
-        else if (bigNum >= bigBorder2 && smallNum >= smallBorder1 && border == true)
+        else if (bigNum >= bigBorder3 && smallNum >= smallBorder1 && border == true)
         {
             missionSlide.Play();
-            mission.text = splitText[2];
-            submis.text = splitText[3];
+            mission.text = splitText[3];
+            submis.text = splitText[4];
+            exmis.text = splitText[5];
             count.text = "2";
             border = false;
             check = true;
         }
-        else if (bigNum >= bigBorder1 && smallNum >= smallBorder2 && border == true)
+        else if (bigNum >= bigBorder2 && smallNum >= smallBorder2 && border == true)
         {
             missionSlide.Play();
-            mission.text = splitText[2];
-            submis.text = splitText[3];
+            mission.text = splitText[3];
+            submis.text = splitText[4];
+            exmis.text = splitText[5];
             count.text = "2";
             border = false;
             check = true;
         }
-        else if (smallNum >= smallBorder3 && border == true)
+        else if (bigNum >= bigBorder1 && smallNum >= smallBorder3 && border == true)
         {
             missionSlide.Play();
-            mission.text = splitText[2];
-            submis.text = splitText[3];
+            mission.text = splitText[3];
+            submis.text = splitText[4];
+            exmis.text = splitText[5];
             count.text = "2";
             border = false;
             check = true;
-        }
-
-        //これはテスト用、本番は消す
-        if (Input.GetKeyDown(KeyCode.P) && check == true)
-        {
-            missionSlide.Play();
-            mission.text = splitText[4];
-            submis.text = splitText[5];
-            count.text = "3";
-            check = false;
         }
 
         if (dis <= 30 && check)
         {
             missionSlide.Play();
-            mission.text = splitText[4];
-            submis.text = splitText[5];
+            mission.text = splitText[6];
+            submis.text = splitText[7];
+            exmis.text = splitText[8];
             count.text = "3";
             check = false;
         }
