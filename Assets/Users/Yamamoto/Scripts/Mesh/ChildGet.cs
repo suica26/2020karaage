e8131c43d65eefdb6bfbc8bc5018d9f3ab84c1f0
suica26 <m011931038@edu.teu.ac.jp>
2021-06-24 22:48:35 +0900

    MeshCutなどなどいろいろお試し 小物が壊れた際にColliderがそのままになるように調整&地面に接触したら音が鳴るように改変

---
+++ Assets/Users/Yamamoto/Scripts/Mesh/ChildGet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildGet : MonoBehaviour
+{
+    public GameObject[] children;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        children = new GameObject[transform.childCount];
+
+        if (transform.childCount > 0)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                children[i] = transform.GetChild(i).gameObject;
+            }
+        }
+    }
+}
