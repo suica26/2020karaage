39c770b57b4e63ea4d95a7c033423db22f4bf0c5
Kinoshita Ryoki <m01191002e@edu.teu.ac.jp>
2021-06-25 21:30:38 +0900

    プレイヤーの周囲オブジェクトを取得（サウンド用）、St.1触った、ObjectStateManagement_Yに加筆

---
+++ Assets/ADX/Script/ADX_FootStepsControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ADX_FootStepsControl : MonoBehaviour
+{
+    public new CriAtomSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = (CriAtomSource)GetComponent("CriAtomSource");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            audio.player.SetSelectorLabel("Selector_Floor", "soil");
+
+        }
+        else if (other.gameObject.tag == "Wood")
+        {
+            audio.player.SetSelectorLabel("Selector_Floor", "wood");
+        }
+        else
+        {
+            //audio.player.SetSelectorLabel("Selector_Floor", "asphalt");
+        }
+    }
+}
---
+++ Assets/ADX/Script/AISAC_Con_Tset.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISAC_Con_Tset : MonoBehaviour
+{
+    private float BGM_AISACNUM;
+    public CriAtomSource bgmCriAtomSource;//BGMのCriAtomSourceアタッチしないと効かない
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetBGMControlValue(0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Invoke("Wait", 1f);
+        }
+            AISACDown();
+        //Debug.Log(BGM_AISACNUM);
+    }
+    private void Wait()
+    {
+
+        AISACUp();
+    }
+
+
+    //ADX　Aisac値変更
+    private void SetBGMControlValue(float value)
+    {
+        bgmCriAtomSource.SetAisacControl("BGM_Aisac", value);
+    }
+    //ADX　時間経過でAisac値下降
+    private void AISACDown()
+    {
+        if (BGM_AISACNUM >= 0.0f)
+        {
+            BGM_AISACNUM -= Time.deltaTime / 20f;
+            SetBGMControlValue(BGM_AISACNUM);
+            
+        }
+    }
+    //ADX　Aisac値上昇
+    private void AISACUp()
+    {
+        if (BGM_AISACNUM <= 1.0f)
+        {
+            //Debug.Log("AISACChange");
+            BGM_AISACNUM += 0.05f;
+            SetBGMControlValue(BGM_AISACNUM);
+        }
+    }
+}
