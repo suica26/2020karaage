fc3f0bf1619f1a54c0bf35691a1b334af6689421
Kinoshita Ryoki <m01191002e@edu.teu.ac.jp>
2021-06-29 23:22:58 +0900

    環境音調整

--- Assets/ADX/Script/ADX_Ray_Rev.cs
+++ Assets/ADX/Script/ADX_Ray_Rev.cs
@@ -10,13 +10,18 @@ public class ADX_Ray_Rev : MonoBehaviour
     private float RevSendLev_R, RevSendLev_L;
     private Text RevSendLevel;
     public GameObject Debug_RevSendLevel, HitPoint, SoundDebudPanel;
-    private bool A;
-    private float a, b, c, d, e, f, g, h, i, j,k ,l, m;
+    private bool A, Once, OnGround;
+    private float a, b, c, d, e, f, g, h, i, j, k, l, m, o, p, q, r, TownNoizeNum, Num, TownNoizeEQNum, EQNum;
+    private float Ypos;
     private bool DebugMode;
 
     public float span = 0f;
     private float currentTime = 0f;
     private float True_RevSendLev;
+
+    private CriAtomSource Sound;
+    public CriAtomSource TownNoizeCon;
+    private string GroundMaterial;
 
     //プロパティー
     public float ADX_BusSendLevel_L
@@ -35,8 +40,10 @@ public class ADX_Ray_Rev : MonoBehaviour
     {
         this.RevSendLevel = Debug_RevSendLevel.GetComponent<Text>();
         DebugMode = false;
-        //SoundDebudPanel.gameObject.SetActive(false);
-
+        Sound = GetComponent<CriAtomSource>();
+        Once = true;
+        //NoizeControl AISAC
+        TownNoizeNum = 0.0f;
     }
 
     // Update is called once per frame
@@ -55,18 +62,82 @@ public class ADX_Ray_Rev : MonoBehaviour
             f = Rays(1, 0, -1, 8);//右後
             g = Rays(-1, 0, 1, 8);//左前
             h = Rays(-1, 0, -1, 8);//左後
-            i = Rays(0, -1, 0, 8);//下
+            i = Rays(0, -1, 0, 0.1f);//下
             j = Rays(1, -1, 1, 8);//右前下
             k = Rays(1, -1, -1, 8);//右後下
             l = Rays(-1, -1, 1, 8);//左前下
             m = Rays(-1, -1, -1, 8);//左後下
+            o = Rays(-1, 1, -1, 8);//左後上
+            p = Rays(1, 1, 1, 8);//右前上
+            q = Rays(1, 1, -1, 8);//右後上
+            r = Rays(-1, 1, 1, 8);//左前上
 
             RevSendLev_L = (a + e + f + j + k) / 10;//Lch_SendLevel
             RevSendLev_R = (b + g + h + l + m) / 10;//Rch_SendLevel
 
-            this.RevSendLevel.text = "RevSendLevel_L:" + RevSendLev_L + "RevSendLevel_R:" + RevSendLev_R;
+
             currentTime = 0f;
+
+            //TownNoizeNum = - (a + b + c + d + e + f + g + h + o + p + q + r)/8;
+
+            //木にレイがあったていれば
+            if (a == -1 | b == -1 | c == -1 | d == -1 | e == -1 | f == -1 | g == -1 | h == -1 | j == -1 | k == -1 | l == -1 | m == -1 | o == -1 | p == -1 | q == -1 | r == -1)
+            {
+                if (TownNoizeNum <= 1.0f)
+                {
+                    TownNoizeNum += 0.1f;
+                }
+            }
+            else
+            {
+                if (TownNoizeNum >= 0.0f)
+                {
+                    TownNoizeNum -= 0.1f;
+                }
+            }
+
+            TownNoizeEQNum = (a + b + c + d + e + f + g + h + j + k + l + m + o + p + q + r) / 20;
+
+            if(TownNoizeEQNum >= 0.55f)
+            {
+                if (EQNum <= 1.0f)
+                {
+                    EQNum += 0.15f;
+                }
+            }
+            else
+            {
+                if (EQNum >= 0.0f)
+                {
+                    EQNum -= 0.15f;
+                }
+            }
+
+
+
+            //NoizeControl AISAC
+            TownNoizeCon.SetAisacControl("NoizeControl", TownNoizeNum);
+            //NoizeEQControl AISAC
+            TownNoizeCon.SetAisacControl("NoizeEQControl", EQNum);
+
+
         }
+        //高さ座標からAISAC値を決定
+        Vector3 Pos = this.transform.position;
+        Ypos = Pos.y / 24.0f;
+        //Obj_angle AISAC
+        TownNoizeCon.SetAisacControl("Obj_angle", Ypos);
+
+            this.RevSendLevel.text = "RevSendLevel_L:" + RevSendLev_L + "RevSendLevel_R:" + RevSendLev_R + "\n" + "TownNoise:" + TownNoizeNum + "EQ:" + EQNum + "\n" + "Pos:" + Ypos;
+        //接地＆材質判定
+        OnGround = GloundRays(0.5f);
+
+        if (OnGround == true & Once == false)
+        {
+            Sound.Play("Landing");
+            Once = true;
+        }
+        Sound.player.SetSelectorLabel("Selector_Floor", GroundMaterial);
 
         //デバッグモードONOFF
         if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.L) & DebugMode == false)
@@ -79,8 +150,6 @@ public class ADX_Ray_Rev : MonoBehaviour
             DebugMode = false;
             SoundDebudPanel.gameObject.SetActive(false);
         }
-
-
     }
 
     //RayCastメソッド
@@ -88,40 +157,71 @@ public class ADX_Ray_Rev : MonoBehaviour
     {
         //Rayの作成　　　　　　　Rayを飛ばす原点　　                                  　,飛ばす方向
         Ray ray = new Ray(transform.position + new Vector3(0.0f, 1.0f, 0.0f), transform.rotation * new Vector3(X, Y, Z));
-
         //Rayが当たったオブジェクトの情報を収納
         RaycastHit hit;
-
-
         //Rayの可視化 
         Debug.DrawRay(ray.origin, ray.direction * distance, Color.yellow);
 
         //もしRayにオブジェクトが衝突したら
         if (Physics.Raycast(ray, out hit, distance))
         {
-            if (hit.collider.tag == "Building")
+            if (hit.collider.tag == "Big")
             {
-                if (DebugMode == true)//デバッグモード
-                {
-                    //可視化点生成
-                    GameObject Spere = Instantiate(HitPoint, new Vector3(0, -10, 0), Quaternion.identity);
-                    //可視化点移動
-                    Spere.transform.position = hit.point;
-                    //ｎフレーム後に実行する//可視化点削除
-                    StartCoroutine(DelayMethod(8, () => { Destroy(Spere); }));
-                }
-                A = true;
+                Num = 1f;
             }
 
+            if (hit.collider.tag == "Tree")
+            {
+                Num = -1f;
+            }
+
+            if (DebugMode == true)//デバッグモード
+            {
+                //可視化点生成
+                GameObject Spere = Instantiate(HitPoint, new Vector3(0, -10, 0), Quaternion.identity);
+                //可視化点移動
+                Spere.transform.position = hit.point;
+                //ｎフレーム後に実行する//可視化点削除
+                StartCoroutine(DelayMethod(8, () => { Destroy(Spere); }));
+            }
         }
         else
         {
-            A = false;
+            Num = 0f;
         }
-
         //Aがtrueなら返り値は　1
         //Aがfalseなら返り値は　0
-        return A ? 1f : 0f;
+        return Num;
+    }
+
+    //接地RayCastメソッド
+    private bool GloundRays(float Glounddistance)
+    {
+        //Rayの作成　　　　
+        Ray Gray = new Ray(transform.position, Vector3.down);
+        RaycastHit Ghit;
+        //Rayの可視化 
+        Debug.DrawRay(Gray.origin, Gray.direction * Glounddistance, Color.black);
+        //Rayにオブジェクトが衝突したら
+        if(Physics.Raycast(Gray, out Ghit, Glounddistance))
+        {
+            OnGround = true;
+
+            if (Ghit.collider.tag == "Soil")
+            {
+                GroundMaterial = "soil";
+            }
+            else
+            {
+                GroundMaterial = "asphalt";
+            }
+        }
+        else
+        {
+            OnGround = false;
+            Once = false;
+        }
+        return OnGround;
     }
 
     //コルーチン
