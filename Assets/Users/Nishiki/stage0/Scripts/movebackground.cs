71945163eb88f29f21094eac6db3243f83895dbf
nishiki3160 <65889937+nishiki3160@users.noreply.github.com>
2021-06-30 20:50:12 +0900

    ステージ０　背景動かしてみた

---
+++ Assets/Users/Nishiki/stage0/Scripts/movebackground.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class movebackground : MonoBehaviour
+{
+	
+	public float speed = 4.0f;
+
+	public float px;
+	public float py;
+	public float pz;
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+		transform.position += transform.right * speed * Time.deltaTime;
+		
+		
+	}
+	void OnTriggerEnter(Collider collision)
+	{
+
+		if (collision.gameObject.name == "backgroundhiter")
+		{
+
+			transform.position = new Vector3(px, py, pz);
+		}
+	}
+}
