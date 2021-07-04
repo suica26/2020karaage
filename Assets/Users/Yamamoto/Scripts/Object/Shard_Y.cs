60bc5d33bd4a329c27d4867015adcb1f1b4d8de7
suica26 <m011931038@edu.teu.ac.jp>
2021-07-04 16:50:46 +0900

    車のメリット(破壊されたら吹っ飛んでってオブジェクトワンパン) 連鎖破壊実装

---
+++ Assets/Users/Yamamoto/Scripts/Object/Shard_Y.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shard_Y : MonoBehaviour
+{
+    public int shardDamage;
+    private bool gaveDamage = false;
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!gaveDamage)
+        {
+            var objScr = other.gameObject.GetComponent<ObjectStateManagement_Y>();
+            if (objScr != null)
+            {
+                objScr.HP -= shardDamage;
+                objScr.SetSkillID(0);
+                objScr.LivingCheck();
+                gaveDamage = true;
+            }
+        }
+    }
+}
