057f9c3bc038bffd727a6c608c6b22c0b0f1ccce
suica26 <m011931038@edu.teu.ac.jp>
2021-08-05 17:38:59 +0900

    WayPointの配置完了 動的NavMesh

---
+++ Assets/Users/Yamamoto/Scripts/Enemy/BuildDynamicNavMesh.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[DefaultExecutionOrder(-103)]
+public class BuildDynamicNavMesh : MonoBehaviour
+{
+    private NavMeshSurface _surface;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _surface = GetComponent<NavMeshSurface>();
+        StartCoroutine(TimeUpdate());
+    }
+
+    IEnumerator TimeUpdate()
+    {
+        while (true)
+        {
+            _surface.BuildNavMesh();
+
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+}
