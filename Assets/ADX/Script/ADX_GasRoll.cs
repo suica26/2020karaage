5af7af5404abd1670d3ce189c07d514514888e4b
Kinoshita Ryoki <m01191002e@edu.teu.ac.jp>
2021-08-19 19:18:33 +0900

    ブラスト、被ダメ、敵　音実装　風音調整

---
+++ Assets/ADX/Script/ADX_GasRoll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ADX_GasRoll : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private CriAtomSource Sound;
+    public float Check　= 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = this.GetComponent<Rigidbody>();
+        Sound = GetComponent<CriAtomSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_rigidbody.velocity.magnitude > Check)
+        {
+            Sound.Play("GasRolling00");
+        }
+    }
+}
