db45bc2cf7c8923f0dcf838a07bde32031c0fbaa
ryuga-masuda <m0119260b1@edu.teu.ac.jp>
2021-09-08 14:19:30 +0900

    タイトルUI調整

---
+++ Assets/Users/Masuda/Script_M/TitleGameStart_M.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleGameStart_M : MonoBehaviour
+{
+    public GameObject gameMode;
+    public string cleared, sceneName;
+
+    void Start()
+    {
+        cleared = PlayerPrefs.GetString("storyClear");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        if (cleared == "clear")
+        {
+            gameMode.SetActive(true);
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
