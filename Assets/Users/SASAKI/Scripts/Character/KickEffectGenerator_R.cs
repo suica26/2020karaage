966325aed97d6bd73a74261a38970c8a818b000a
Yuetatsu <m0119140ba@edu.teu.ac.jp>
2021-09-21 23:46:12 +0900

    エフェクト差し替え(追加分)

---
+++ Assets/Users/SASAKI/Scripts/Character/KickEffectGenerator_R.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KickEffectGenerator_R : MonoBehaviour
+{
+    [SerializeField] private GameObject kickEffect;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Big")
+        {
+            GameObject obj = Instantiate(kickEffect, transform.position, Camera.main.transform.rotation);
+            Destroy(obj, 1.0f);
+        }
+    }
+}
