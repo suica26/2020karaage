93bc5500d7f2ddd48453b697e2d1859d3214ed64
suica26 <m011931038@edu.teu.ac.jp>
2021-09-24 16:50:39 +0900

    敵の巡回script完成

--- Assets/Users/Yamamoto/Scripts/Enemy/EnemyStatusForGameAI.cs
+++ Assets/Users/Yamamoto/Scripts/Enemy/EnemyStatusForGameAI.cs
@@ -4,17 +4,11 @@ using UnityEngine;
 
 public class EnemyStatusForGameAI : MonoBehaviour
 {
-    private EnemySpawnController AIscript;
+    public EnemySpawnController AIscript;
     public int i;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        AIscript = GameObject.Find("GameAI_Y").GetComponent<EnemySpawnController>();
-    }
 
     private void OnDestroy()
     {
-        AIscript.UpdateEnemyNum(i);
+        AIscript?.UpdateEnemyNum(i);
     }
 }
