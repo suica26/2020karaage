f827d87a8a85d5e623b8872048af7c03f94debdf
Kinoshita Ryoki <m01191002e@edu.teu.ac.jp>
2021-09-27 02:02:43 +0900

    モバイル設定済

--- Assets/Users/SASAKI/Scripts/Mobile/MobileSetting_R.cs
+++ Assets/Users/SASAKI/Scripts/Mobile/MobileSetting_R.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MobileSetting_R : MonoBehaviour
 {
     // ここを書き換えるとモバイルと通常版の操作を切り替えられます
-    private bool mobileMode = false;
+    private bool mobileMode = true;
 
     private static MobileSetting_R instance = new MobileSetting_R();
 
