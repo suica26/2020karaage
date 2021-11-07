ffb8a06d13f02858aad8ccc8054601ca5d0a97ef
suica26 <m011931038@edu.teu.ac.jp>
2021-11-07 14:56:44 +0900

    ライト変化

---
+++ Assets/Users/Yamamoto/Scripts/Etcetra/ScoreAttackLight_Y.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreAttackLight_Y : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreAttack_Y.directionalLight = gameObject;
+    }
+}
