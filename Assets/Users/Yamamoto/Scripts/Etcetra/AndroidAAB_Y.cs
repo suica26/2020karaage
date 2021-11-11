325391f604f2ffecf9064a4b592e52f6a8af535c
suica26 <m011931038@edu.teu.ac.jp>
2021-11-11 14:52:24 +0900

    GoogleのPluginをインポートしなおし

---
+++ Assets/Users/Yamamoto/Scripts/Etcetra/AndroidAAB_Y.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Google.Play.AssetDelivery;
+
+public class AndroidAAB_Y : MonoBehaviour
+{
+    private void LoadAsset1(string assetBundleName, string fileName)
+    {
+        var bundleRequest = PlayAssetDelivery.RetrieveAssetBundleAsync(assetBundleName);
+        bundleRequest.Completed += request =>
+        {
+            if (request.Status == AssetDeliveryStatus.Loaded ||
+            request.Status == AssetDeliveryStatus.Available)
+            {
+                var prefab = request.AssetBundle.LoadAsset<GameObject>(fileName);
+                Instantiate(prefab);
+            }
+        };
+    }
+    // 異なる場合は別途指定
+    private void LoadAsset2(string assetPackName, string assetBundlePath, string fileName)
+    {
+        var packRequest = PlayAssetDelivery.RetrieveAssetPackAsync(assetPackName);
+        packRequest.Completed += request =>
+        {
+            if (request.Status == AssetDeliveryStatus.Loaded ||
+            request.Status == AssetDeliveryStatus.Available)
+            {
+                var bundleCreateRequest = packRequest.LoadAssetBundleAsync(assetBundlePath);
+                bundleCreateRequest.completed += _ =>
+                {
+                    var prefab = bundleCreateRequest.assetBundle.LoadAsset<GameObject>(fileName);
+                    Instantiate(prefab);
+                };
+            };
+        };
+    }
+}
